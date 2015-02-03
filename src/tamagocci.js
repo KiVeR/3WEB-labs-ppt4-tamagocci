377cf9bbfa9385e3f08331aa259688cd4277cbb7
Nicolas Bossaton <bossaton.nicolas@gmail.com>
2015-02-03 19:51:32 +0100

    base2

--- src/tamagocci.js
+++ src/tamagocci.js
@@ -44,3 +44,31 @@ Tamagocci.prototype.getPicture = function(){
   }
   return 'pk_good.gif';
 }
+
+/*
+* PIKACHU
+* */
+function Pikachu() {
+  Tamagocci.apply(this);
+}
+
+Pikachu.prototype = Object.create(Tamagocci.prototype);
+
+/*
+* HelloKitty
+* */
+function HelloKitty() {
+  Tamagocci.apply(this);
+}
+
+HelloKitty.prototype = Object.create(Tamagocci.prototype);
+
+HelloKitty.prototype.getPicture = function(){
+  if (this.happiness < 3 && this.happiness > 0 || this.weight == this.maxWeight - 2 || this.weight == this.minWeight + 2) {
+    return 'hk_bad.gif';
+  }
+  if(this.happiness == 0){
+    return 'hk_dead.png';
+  }
+  return 'hk_good.gif';
+}
